f1dd4fb61fa4461e0f3ce66e1f3c70ce06dd8230
Jules <102732657+JulesChe@users.noreply.github.com>
2023-12-08 00:24:08 +0100

    fix

--- script/pop_up_prejuge.js
+++ script/pop_up_prejuge.js
@@ -1,85 +1,189 @@
-let upgrade_click = document.getElementById('upgrade_button')
+let upgrade_button = document.getElementById('upgrade_button')
+
+upgrade_button.addEventListener('click', handleUpgradeButton);
+
+function handleUpgradeButton() {
+    createQuestionModal()
+    updateModalQuestion()
+}
 
 
-upgrade_click.addEventListener('click', function (e) {
+function createQuestionModal() {
+    // Création du div modal
+    var modal = document.createElement("div");
+    modal.classList.add("modal");
+    modal.setAttribute("id", "modal");
 
-    if (isUpgradable()) {
-        let prejuge = getQuestion()
+    // Création du fond du modal (backdrop)
+    var modalBack = document.createElement("div");
+    modalBack.classList.add("modal-back");
+    modal.appendChild(modalBack);
 
-        let modal = document.getElementById('modal')
+    // Création du conteneur du modal
+    var modalContainer = document.createElement("div");
+    modalContainer.classList.add("modal-container");
+    modal.appendChild(modalContainer);
 
-        let modal_reponse = document.getElementById('modal_reponse')
+    // Création du titre (h1)
+    var title = document.createElement("h1");
+    title.textContent = "Cette affirmation est-elle vraie ?";
+    modalContainer.appendChild(title);
 
-        document.getElementById('question_modal').textContent = prejuge.titre
+    // Création du paragraphe pour la question
+    var questionParagraph = document.createElement("p");
+    questionParagraph.setAttribute("id", "question_modal");
+    questionParagraph.textContent = "question";
+    modalContainer.appendChild(questionParagraph);
 
-        modal.style.display = 'block'
+    var trueButton = document.createElement("button");
+    trueButton.setAttribute("id", "button_true_answer");
+    trueButton.textContent = "Vrai";
+    modalContainer.appendChild(trueButton);
+    trueButton.addEventListener('click', handleTrueButton); // Attacher le gestionnaire ici
 
-        let true_button = document.getElementById('button_true_answer')
+    var falseButton = document.createElement("button");
+    falseButton.setAttribute("id", "button_false_answer");
+    falseButton.textContent = "Faux";
+    modalContainer.appendChild(falseButton);
+    falseButton.addEventListener('click', handleFalseButton); // Attacher le gestionnaire ici
 
-        let false_button = document.getElementById('button_false_answer')
+
+    // Création du lien pour fermer le modal
+    var closeModalLink = document.createElement("a");
+    closeModalLink.setAttribute("href", "#");
+    closeModalLink.setAttribute("id", "modal-close");
+    closeModalLink.textContent = "[fermer]";
+
+    closeModalLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        modal.remove()
+    });
 
 
-        true_button.addEventListener('click', function (e) {
+    modalContainer.appendChild(closeModalLink);
 
-            modal.style.display = 'none'
+    // Ajout du modal à la fin du body
+    document.body.appendChild(modal);
 
-            let solution = document.getElementById('solution')
+}
 
-            if (prejuge.solution == true) {
+function updateModalQuestion(){
 
-                solution.textContent = "Bravo !"
+    let modal_question = document.getElementById('question_modal')
+    modal_question.textContent = questionJSONArr[treeState].titre
+}
 
-                upgradeTree()
 
-            } else {
+function createReponseModal(titleText, responseText) {
+    // Vérifier si la modale de réponse existe déjà
+    var existingModal = document.getElementById('modal_reponse');
+    if (!existingModal) {
+        // Création de la modale de réponse si elle n'existe pas
+        var modal = document.createElement("div");
+        modal.classList.add("modal");
+        modal.setAttribute("id", "modal_reponse");
 
-                solution.textContent = "Tu as perdu !"
+        // Création du fond du modal (backdrop)
+        var modalBack = document.createElement("div");
+        modalBack.classList.add("modal-back");
+        modal.appendChild(modalBack);
 
-                wrongAnswer()
-            }
+        // Création du conteneur du modal
+        var modalContainer = document.createElement("div");
+        modalContainer.classList.add("modal-container");
+        modal.appendChild(modalContainer);
 
-            let reponse = document.getElementById('reponse')
+        // Création du titre (h1)
+        var title = document.createElement("h1");
+        title.setAttribute("id", "solution");
+        title.textContent = titleText; // Utilisation du paramètre pour le titre
+        modalContainer.appendChild(title);
 
-            reponse.textContent = prejuge.reponse
+        // Création du paragraphe pour la réponse
+        var response = document.createElement("p");
+        response.setAttribute("id", "reponse");
+        response.textContent = responseText; // Utilisation du paramètre pour le texte de réponse
+        modalContainer.appendChild(response);
 
-            modal_reponse.style.display = 'block'
-
+        // Création du lien pour fermer le modal
+        var closeModal = document.createElement("a");
+        closeModal.setAttribute("href", "#");
+        closeModal.setAttribute("id", "modal-close_reponse");
+        closeModal.textContent = "[fermer]";
+        closeModal.addEventListener('click', function (e) {
+            e.preventDefault();
+            modal.remove()
         });
+        modalContainer.appendChild(closeModal);
 
-        false_button.addEventListener('click', function (e) {
+        // Ajout du modal à la fin du body
+        document.body.appendChild(modal);
 
-            modal.style.display = 'none'
 
-            let solution = document.getElementById('solution')
+    } else {
+        // Si la modale existe déjà, mettre à jour le contenu et afficher la modale
+        existingModal.querySelector("#solution").textContent = titleText;
+        existingModal.querySelector("#reponse").textContent = responseText;
+        existingModal.style.display = 'block';
+    }
+}
 
-            if (prejuge.solution == false) {
 
-                solution.textContent = "Bravo !"
 
-                upgradeTree()
 
-            } else {
 
-                solution.textContent = "Tu as perdu !"
+function closeModal() {
+    var modal = document.getElementById('modal');
+    modal.remove()
+    if (modal) {
+        modal.style.display = 'none';
+    }
+    var modalReponse = document.getElementById('modal_reponse');
+    modalReponse.remove()
+    if (modalReponse) {
+        modalReponse.style.display = 'none';
+    }
 
-                wrongAnswer()
-            }
 
-            let reponse = document.getElementById('reponse')
+}
 
-            reponse.textContent = prejuge.reponse
 
-            modal_reponse.style.display = 'block'
+// Gestionnaire pour le true_button
+function handleTrueButton() {
 
-        });
+    if(questionJSONArr[treeState].solution == true){
+
+        createReponseModal("Bravo !",questionJSONArr[treeState].reponse)
+        upgradeTree()
+
+    }else {
+
+        createReponseModal("Tu as perdu !",questionJSONArr[treeState].reponse)
+        wrongAnswer()
 
     }
-});
+    var modal = document.getElementById('modal');
+    modal.remove()
 
-document.getElementById('modal-close').addEventListener('click', function (e) {
-    document.getElementById('modal').style.display = 'none'
-})
+}
 
-document.getElementById('modal-close_reponse').addEventListener('click', function (e) {
-    document.getElementById('modal_reponse').style.display = 'none'
-})
+// Gestionnaire pour le false_button
+function handleFalseButton() {
+
+    if(questionJSONArr[treeState].solution == false){
+
+        createReponseModal("Bravo !",questionJSONArr[treeState].reponse)
+        upgradeTree()
+
+    }else {
+
+        createReponseModal("Tu as perdu !",questionJSONArr[treeState].reponse)
+        wrongAnswer()
+
+    }
+
+    var modal = document.getElementById('modal');
+    modal.remove()
+
+
+}
