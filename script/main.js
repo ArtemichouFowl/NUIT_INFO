39052f4ee99f42769f8c13b778aea6bd69578411
ArtemichouFowl <104560355+ArtemichouFowl@users.noreply.github.com>
2023-12-07 21:44:35 +0100

    Merge correctif

--- script/main.js
+++ script/main.js
@@ -11,28 +11,28 @@ let upgradeTag = document.getElementById("upgrade_button") // text faire pousser
 let treeImage = document.getElementById("tree_img")// image arbre
 
 let questionJSONArr = [{
-      "titre": "Les scientifiques sont tous d'accord",
-      "reponse": "Le GIEC (Groupe d’experts intergouvernemental sur l’évolution du climat), formé par les Nations Unies, étudie depuis une vingtaine d’années les différents travaux des scientifiques du monde entier. Tous les rapports de cet organisme indiquent la dangerosité du réchauffement climatique, provoqué par l’homme et les émissions de gaz à effet de serre.",
-      "solution": true
-    },
-    {
-      "titre": "Le changement climatique peut être résolu par les progrès scientifiques",
-      "reponse": "Les progrès technologiques pourront peut-être dans un futur lointain nous permettre de coloniser de nouvelles planètes, comme solution de sauvegarde de l’espèce humaine… Mais aucune découverte scientifique majeure n’a pu influer sur le climat.",
-      "solution": false
-    },
-    {
-      "titre": "Le réchauffement climatique est limité",
-      "reponse": "La dernière décennie a été considérée comme la plus chaude depuis le début des relevés de températures. Le réchauffement climatique risque de provoquer des records de chaleur à l’avenir et de provoquer une augmentation des réfugiés climatiques, face à la sécheresse et à la pénurie d’eau.",
-      "solution": false
-    },{
-      "titre": "La France sera peu touchée par le changement climatique",
-      "reponse": "Il s’agit d’une idée reçue : si le réchauffement climatique va provoquer des catastrophes humanitaires dans les régions déjà arides, les pays plus tempérés comme la France en subiront aussi les effets. La destruction de la faune et de la flore de nombreuses régions, telles que les vignes du Bordelais ou les espèces végétales en haute montagne, pourrait advenir.",
-      "solution": false
-    },{
-      "titre": "Les politiques écologiques ne suffisent pas à endiguer le problème",
-      "reponse": "L’engagement de la communauté internationale à limiter le réchauffement climatique à moins de 2 degrés est un pas important. Mais les mesures écologiques plus ambitieuses comme l’abandon des énergies fossiles ne sont pas encore envisagées au niveau mondial.",
-      "solution": true
-    }]
+    "titre": "Les scientifiques sont tous d'accord",
+    "reponse": "Le GIEC (Groupe d’experts intergouvernemental sur l’évolution du climat), formé par les Nations Unies, étudie depuis une vingtaine d’années les différents travaux des scientifiques du monde entier. Tous les rapports de cet organisme indiquent la dangerosité du réchauffement climatique, provoqué par l’homme et les émissions de gaz à effet de serre.",
+    "solution": true
+},
+{
+    "titre": "Le changement climatique peut être résolu par les progrès scientifiques",
+    "reponse": "Les progrès technologiques pourront peut-être dans un futur lointain nous permettre de coloniser de nouvelles planètes, comme solution de sauvegarde de l’espèce humaine… Mais aucune découverte scientifique majeure n’a pu influer sur le climat.",
+    "solution": false
+},
+{
+    "titre": "Le réchauffement climatique est limité",
+    "reponse": "La dernière décennie a été considérée comme la plus chaude depuis le début des relevés de températures. Le réchauffement climatique risque de provoquer des records de chaleur à l’avenir et de provoquer une augmentation des réfugiés climatiques, face à la sécheresse et à la pénurie d’eau.",
+    "solution": false
+}, {
+    "titre": "La France sera peu touchée par le changement climatique",
+    "reponse": "Il s’agit d’une idée reçue : si le réchauffement climatique va provoquer des catastrophes humanitaires dans les régions déjà arides, les pays plus tempérés comme la France en subiront aussi les effets. La destruction de la faune et de la flore de nombreuses régions, telles que les vignes du Bordelais ou les espèces végétales en haute montagne, pourrait advenir.",
+    "solution": false
+}, {
+    "titre": "Les politiques écologiques ne suffisent pas à endiguer le problème",
+    "reponse": "L’engagement de la communauté internationale à limiter le réchauffement climatique à moins de 2 degrés est un pas important. Mais les mesures écologiques plus ambitieuses comme l’abandon des énergies fossiles ne sont pas encore envisagées au niveau mondial.",
+    "solution": true
+}]
 
 updateUpgradeTag()
 updateScoreTag();
@@ -40,40 +40,42 @@ updateScoreTag();
 
 document.addEventListener('DOMContentLoaded', startFallingLeaves);
 
+//click sur le tree -> augmente le score
 treeImage.addEventListener("click", function () {
     increaseScore();
-    playAudio();
 });
 
-upgradeTag.addEventListener("click", function () {
-    upgradeTree();
-});
 
+
+//augmente le score par rapport a l'etat de l'arbre
 function increaseScore() {
     score += addValue[treeState]
     updateScoreTag();
 }
 
 //fausse reponse a l'upgrade
-function wrongAnswer(){
-    score-=upgradePrices[treeState]
+function wrongAnswer() {
+    score -= addValue[treeState]
+    score -= upgradePrices[treeState]
     updateScoreTag()
 }
 
 //bonne reponse a l'upgrade
-function upgradeTree(){
-    if(treeState<finalState && score >= upgradePrices[treeState]){
-        score-=upgradePrices[treeState]
+function upgradeTree() {
+    if (treeState < finalState && score >= upgradePrices[treeState]) {
+        score -= upgradePrices[treeState]
         treeState++
     }
     updateScoreTag()
     updateUpgradeTag()
 }
 
+//met a jour le score
 function updateScoreTag() {
     scoreTag.innerHTML = score;
 }
 
+//met a jour le bouton d'amélioration
 function updateUpgradeTag() {
     if (treeState < finalState) {
         upgradeTag.innerHTML = `Faire pousser l'arbre : ${upgradePrices[treeState]}`
@@ -81,6 +83,17 @@ function updateUpgradeTag() {
         upgradeTag.innerHTML = "Vous avez terminé !"
     }
 }
+
+
+function isUpgradable() {
+    return (score >= upgradePrices[treeState])
+}
+
+function getQuestion() {
+    return questionJSONArr[treeState]
+}
+
+
 
 function playAudio() {
     let audio = document.getElementById("clickMusic")
--- script/pop_up_prejuge.js
+++ script/pop_up_prejuge.js
@@ -1,84 +1,85 @@
 let upgrade_click = document.getElementById('upgrade_button')
 
 
-upgrade_click.addEventListener('click', function(e) {
-    if(isUpgradable()){
-      let prejuge = getQuestion()
+upgrade_click.addEventListener('click', function (e) {
 
-      let modal = document.getElementById('modal')
-  
-      let modal_reponse = document.getElementById('modal_reponse')
-  
-      document.getElementById('question_modal').textContent = prejuge.titre
-  
-      modal.style.display = 'block'
-  
-      let true_button = document.getElementById('button_true_answer')
-  
-      let false_button = document.getElementById('button_false_answer')
-  
-  
-      true_button.addEventListener('click', function(e) {
-  
-          modal.style.display = 'none'
-  
-          let solution = document.getElementById('solution')
-  
-          if(prejuge.solution == true){
-  
-              solution.textContent = "Bravo !"
-  
-              upgradeTree()
-  
-          } else {
-  
-              solution.textContent = "Tu as perdu !"
-  
-              wrongAnswer()
-          }
-  
-          let reponse = document.getElementById('reponse')
-  
-          reponse.textContent = prejuge.reponse
-  
-          modal_reponse.style.display = 'block'
-  
-      });
-  
-      false_button.addEventListener('click', function(e) {
-  
-          modal.style.display = 'none'
-  
-          let solution = document.getElementById('solution')
-  
-          if(prejuge.solution == false){
-  
-              solution.textContent = "Bravo !"
-  
-              upgradeTree()
-  
-          } else {
-  
-              solution.textContent = "Tu as perdu !"
-  
-              wrongAnswer()
-          }
-  
-          let reponse = document.getElementById('reponse')
-  
-          reponse.textContent = prejuge.reponse
-  
-          modal_reponse.style.display = 'block'
-  
-      });
-  
+    if (isUpgradable()) {
+        let prejuge = getQuestion()
+
+        let modal = document.getElementById('modal')
+
+        let modal_reponse = document.getElementById('modal_reponse')
+
+        document.getElementById('question_modal').textContent = prejuge.titre
+
+        modal.style.display = 'block'
+
+        let true_button = document.getElementById('button_true_answer')
+
+        let false_button = document.getElementById('button_false_answer')
+
+
+        true_button.addEventListener('click', function (e) {
+
+            modal.style.display = 'none'
+
+            let solution = document.getElementById('solution')
+
+            if (prejuge.solution == true) {
+
+                solution.textContent = "Bravo !"
+
+                upgradeTree()
+
+            } else {
+
+                solution.textContent = "Tu as perdu !"
+
+                wrongAnswer()
+            }
+
+            let reponse = document.getElementById('reponse')
+
+            reponse.textContent = prejuge.reponse
+
+            modal_reponse.style.display = 'block'
+
+        });
+
+        false_button.addEventListener('click', function (e) {
+
+            modal.style.display = 'none'
+
+            let solution = document.getElementById('solution')
+
+            if (prejuge.solution == false) {
+
+                solution.textContent = "Bravo !"
+
+                upgradeTree()
+
+            } else {
+
+                solution.textContent = "Tu as perdu !"
+
+                wrongAnswer()
+            }
+
+            let reponse = document.getElementById('reponse')
+
+            reponse.textContent = prejuge.reponse
+
+            modal_reponse.style.display = 'block'
+
+        });
+
     }
-  });
+});
 
-document.getElementById('modal-close').addEventListener('click', function(e) {
-document.getElementById('modal').style.display = 'none'
+document.getElementById('modal-close').addEventListener('click', function (e) {
+    document.getElementById('modal').style.display = 'none'
 })
 
-document.getElementById('modal-close_reponse').addEventListener('click', function(e) {
+document.getElementById('modal-close_reponse').addEventListener('click', function (e) {
     document.getElementById('modal_reponse').style.display = 'none'
-    })
+})
