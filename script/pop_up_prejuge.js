4a70492016f55758d854ab6ed62d785df48d437a
Jules <102732657+JulesChe@users.noreply.github.com>
2023-12-07 19:30:54 +0100

    debut pop up

---
+++ script/pop_up_prejuge.js
@@ -0,0 +1,8 @@
+let upgrade_click = document.getElementById('upgrade_click')
+
+
+
+upgrade_click.addEventListener('click', function(e) {
+    
+    document.getElementById('modal').style.display = 'block'
+  });
