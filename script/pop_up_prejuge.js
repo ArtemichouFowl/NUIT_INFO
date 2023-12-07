b83598dbfbb17801c01825d056a1b043826cf7fd
Jules <102732657+JulesChe@users.noreply.github.com>
2023-12-07 19:39:14 +0100

    pop_up squelette

--- script/pop_up_prejuge.js
+++ script/pop_up_prejuge.js
@@ -1,8 +1,11 @@
 let upgrade_click = document.getElementById('upgrade_click')
 
 
-
 upgrade_click.addEventListener('click', function(e) {
     
     document.getElementById('modal').style.display = 'block'
   });
+
+document.getElementById('modal-close').addEventListener('click', function(e) {
+document.getElementById('modal').style.display = 'none'
+})
