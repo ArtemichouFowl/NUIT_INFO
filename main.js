7270d854310c687c4485efdb5dc207ecc9a9dda8
Maxime272003 <62075092+Maxime272003@users.noreply.github.com>
2023-12-08 01:13:06 +0100

    version fonctionnelle

--- main.js
+++ main.js
@@ -2,11 +2,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const game = document.getElementById('game');
     const golden = document.getElementById('key');
     const sections = document.querySelectorAll('.section');
-    const scoreElement = document.getElementById('score');
     const popupElement = document.getElementById('popup');
-    const perduElement = document.getElementById('perdu');
+    const music = document.getElementById('myMusic');
+    let musicData =
+        '0001010101010101000101010001010100010101000101010001010101010101000101010101010100010101010101010001010101010101000101010101010100010001000100010001000110010001000100010001000100010001000100010001010101010101000101010101010100010101010101010001010101010101000101010101010100010101010101010001010101010101000101010101010100010001000100010001000100010001000100010001000100010001000100010001010101010101000101010101010100010101010101010001010101010101000100000000000'.split('')
+    let musicBass = ''.split('')
 
-    let score = 0;
+    let dataIndex = 0;
+    let bpm = 88;
+    let beatDuration = 60000 / bpm;
+    let musicStarted = false;
+
+    startButton.addEventListener('click', function () {
+        startGame(); // Commence la séquence de jeu
+        startButton.style.display = 'none'; // Cache le bouton
+    });
+
+    function startGame() {
+        createRectangle();
+    }
+
     let keyPressedMap = {
         'f': false,
         'g': false,
@@ -20,7 +35,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
         if (!keyPressedMap[key]) {
             keyPressedMap[key] = true;
-            handleKeyPress(key);
         }
     });
 
@@ -32,58 +46,76 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     function createRectangle() {
-        const rectangle = document.createElement('div');
-        rectangle.classList.add('rectangle');
-        rectangle.style.width = sections[0].clientWidth + 'px';
-
-        const randomSectionIndex = Math.floor(Math.random() * sections.length);
-        const selectedSection = sections[randomSectionIndex];
-        rectangle.style.left = selectedSection.offsetLeft + 'px';
-
-        game.appendChild(rectangle);
-
-        let animationRunning = true;
-
-        const animation = rectangle.animate([
-            { top: '-50px' },
-            { top: '100%' }
-        ], {
-            duration: 3000,
-            easing: 'linear'
-        });
-
-        animation.onfinish = function () {
-            animationRunning = false;
-            rectangle.remove();
-        };
-
-        function checkCollision() {
-            if (isRectangleInGolden(rectangle)) {
-                inGoldenZone = true;
-
-                if (keyPressedMap['f'] && randomSectionIndex === 0) {
-                    handleKeyPress('f');
-                    rectangle.classList.add('in-golden');
-                } else if (keyPressedMap['g'] && randomSectionIndex === 1) {
-                    rectangle.classList.add('in-golden');
-                    handleKeyPress('g');
-                } else if (keyPressedMap['h'] && randomSectionIndex === 2) {
-                    rectangle.classList.add('in-golden');
-                    handleKeyPress('h');
-                }
-            } else {
-                rectangle.classList.remove('in-golden');
-                inGoldenZone = false;
-            }
-
-            if (animationRunning) {
-                requestAnimationFrame(checkCollision);
-            }
+        if (dataIndex === 50) { // Exemple : changer le BPM après 50 rectangles
+            bpm = 88; // Nouveau BPM
+            beatDuration = 60000 / bpm;
+            music.playbackRate = 1; // Augmenter la vitesse de lecture de la musique
         }
 
-        checkCollision();
-    }
+        if (musicData[dataIndex] === '1') {
+            const rectangle = document.createElement('div');
+            rectangle.classList.add('rectangle');
+            rectangle.style.width = sections[0].clientWidth + 'px';
 
+            const randomSectionIndex = Math.floor(Math.random() * sections.length);
+            const selectedSection = sections[randomSectionIndex];
+            rectangle.style.left = selectedSection.offsetLeft + 'px';
+
+            game.appendChild(rectangle);
+
+            let animationRunning = true;
+
+            const animation = rectangle.animate([
+                { top: '-50px' },
+                { top: '100%' }
+            ], {
+                duration: 3000,
+                easing: 'linear'
+            });
+
+            animation.onfinish = function () {
+                animationRunning = false;
+                rectangle.remove();
+            };
+
+            function checkCollision() {
+                const rect = rectangle.getBoundingClientRect();
+                const goldenRect = golden.getBoundingClientRect();
+                const distanceToGolden = goldenRect.top - rect.bottom;
+                if (isRectangleInGolden(rectangle)) {
+                    inGoldenZone = true;
+
+                    if (keyPressedMap['f'] && randomSectionIndex === 0) {
+                        rectangle.classList.add('in-golden');
+                    } else if (keyPressedMap['g'] && randomSectionIndex === 1) {
+                        rectangle.classList.add('in-golden');
+                    } else if (keyPressedMap['h'] && randomSectionIndex === 2) {
+                        rectangle.classList.add('in-golden');
+                    }
+                } else {
+                    rectangle.classList.remove('in-golden');
+                    inGoldenZone = false;
+                }
+
+                if (animationRunning) {
+                    requestAnimationFrame(checkCollision);
+                }
+
+                if (distanceToGolden < 50 && !musicStarted) { // 50 est un seuil arbitraire
+                    music.play();
+                    musicStarted = true;
+
+                }
+            }
+
+            checkCollision();
+        }
+
+        dataIndex++;
+        if (dataIndex < musicData.length) {
+            setTimeout(createRectangle, beatDuration);
+        }
+    }
     function isRectangleInGolden(rectangle) {
         const rect = rectangle.getBoundingClientRect();
         const goldenRect = golden.getBoundingClientRect();
@@ -96,16 +128,8 @@ document.addEventListener('DOMContentLoaded', function () {
         );
     }
 
-    function handleKeyPress(key) {
-        if (inGoldenZone) {
-            score++;
-            scoreElement.textContent = `Score: ${score}`;
-        }
-    }
-
     function showPopup() {
         popupElement.style.display = 'block';
     }
 
-    setInterval(createRectangle, 2000);
 });
