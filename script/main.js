d89bfdaae5fbc8308f1cf8a37e316640983f63a6
Jules <102732657+JulesChe@users.noreply.github.com>
2023-12-08 02:24:54 +0100

    maj affirmation

--- script/main.js
+++ script/main.js
@@ -80,29 +80,31 @@ let fruitJSON = [
     }
 ]
 
-let questionJSONArr = [{
-    "titre": "Les scientifiques sont tous d'accord",
-    "reponse": "Le GIEC (Groupe d’experts intergouvernemental sur l’évolution du climat), formé par les Nations Unies, étudie depuis une vingtaine d’années les différents travaux des scientifiques du monde entier. Tous les rapports de cet organisme indiquent la dangerosité du réchauffement climatique, provoqué par l’homme et les émissions de gaz à effet de serre.",
-    "solution": true
-},
-{
-    "titre": "Le changement climatique peut être résolu par les progrès scientifiques",
-    "reponse": "Les progrès technologiques pourront peut-être dans un futur lointain nous permettre de coloniser de nouvelles planètes, comme solution de sauvegarde de l’espèce humaine… Mais aucune découverte scientifique majeure n’a pu influer sur le climat.",
-    "solution": false
-},
-{
-    "titre": "Le réchauffement climatique est limité",
-    "reponse": "La dernière décennie a été considérée comme la plus chaude depuis le début des relevés de températures. Le réchauffement climatique risque de provoquer des records de chaleur à l’avenir et de provoquer une augmentation des réfugiés climatiques, face à la sécheresse et à la pénurie d’eau.",
-    "solution": false
-}, {
-    "titre": "La France sera peu touchée par le changement climatique",
-    "reponse": "Il s’agit d’une idée reçue : si le réchauffement climatique va provoquer des catastrophes humanitaires dans les régions déjà arides, les pays plus tempérés comme la France en subiront aussi les effets. La destruction de la faune et de la flore de nombreuses régions, telles que les vignes du Bordelais ou les espèces végétales en haute montagne, pourrait advenir.",
-    "solution": false
-}, {
-    "titre": "Les politiques écologiques ne suffisent pas à endiguer le problème",
-    "reponse": "L’engagement de la communauté internationale à limiter le réchauffement climatique à moins de 2 degrés est un pas important. Mais les mesures écologiques plus ambitieuses comme l’abandon des énergies fossiles ne sont pas encore envisagées au niveau mondial.",
-    "solution": true
-}]
+let questionJSONArr = [     {
+      "titre": "Les scientifiques sont tous d'accord sur la dangerosité du dérèglement climatique",
+      "reponse": "Le GIEC (Groupe d’experts intergouvernemental sur l’évolution du climat), formé par les Nations Unies, étudie depuis une vingtaine d’années les différents travaux des scientifiques du monde entier. Tous les rapports de cet organisme indiquent la dangerosité du réchauffement climatique, provoqué par l’homme et les émissions de gaz à effet de serre.",
+      "solution": true
+    },
+     {
+      "titre": "Le dérèglement climatique peut être résolu par les progrès scientifiques",
+      "reponse": "Les progrès technologiques pourront peut-être dans un futur lointain nous permettre de coloniser de nouvelles planètes, comme solution de sauvegarde de l’espèce humaine… Mais aucune découverte scientifique majeure n’a pu influer sur le climat.",
+      "solution": false
+    },
+     {
+      "titre": "Les voitures électriques sont plus écologiques que les voitures thermiques",
+      "reponse": "Les voitures électriques sont généralement plus écologiques que les voitures thermiques, principalement parce qu'elles émettent moins de gaz à effet de serre lors de leur utilisation. Cependant, leur fabrication, en particulier celle des batteries, peut être plus polluante.",
+      "solution": false
+    },
+     {
+      "titre": "La France sera peu touchée par le changement climatique",
+      "reponse": "Il s’agit d’une idée reçue : si le réchauffement climatique va provoquer des catastrophes humanitaires dans les régions déjà arides, les pays plus tempérés comme la France en subiront aussi les effets. La destruction de la faune et de la flore de nombreuses régions, telles que les vignes du Bordelais ou les espèces végétales en haute montagne, pourrait advenir.",
+      "solution": false
+    },
+     {
+      "titre": "Les politiques écologiques ne suffisent pas à endiguer le problème",
+      "reponse": "L’engagement de la communauté internationale à limiter le réchauffement climatique à moins de 2 degrés est un pas important. Mais les mesures écologiques plus ambitieuses comme l’abandon des énergies fossiles ne sont pas encore envisagées au niveau mondial.",
+      "solution": true
+    }]
 
 updateUpgradeTag()
 updateScoreTag()
